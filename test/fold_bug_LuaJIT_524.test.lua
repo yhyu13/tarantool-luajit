c9588f51301844d11a2a9dfa9070e437961c9787
Vyacheslav Egorov <vegorov@google.com>
2019-11-25 14:00:51 +0100

    fold: keep type of emitted CONV in sync with its mode
    When emitting CONV make sure that its type matches its destination IRType.
    
    This keeps IR fully internally consistent with respect to types - i.e. if
    we push narrowing CONV Dt.St upwards through an arithmetic operation of type
    St we end up with arithmetic operation of type Dt and two convertions
    CONV Dt.St which narrow the operands.
    
    Igor Munkin:
    * added a test for the problem
    * backported the original patch to tarantool/luajit repo
    * stripped some words not related to the patch itself
    
    Fixes LuaJIT/LuaJIT#524
    
    Signed-off-by: Vyacheslav Egorov <vegorov@google.com>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

---
+++ test/fold_bug_LuaJIT_524.test.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env tarantool
+
+local tap = require('tap')
+local ffi = require('ffi')
+
+local test = tap.test("LuaJIT 524")
+test:plan(1)
+
+-- Test file to demonstrate LuaJIT folding machinery incorrect behaviour,
+-- details:
+--     https://github.com/LuaJIT/LuaJIT/issues/524
+--     https://github.com/moonjit/moonjit/issues/37
+
+jit.opt.start(0, "fold", "cse", "fwd", "hotloop=1")
+
+local sq = ffi.cast("uint32_t", 42)
+
+for _ = 1, 3 do
+    sq = ffi.cast("uint32_t", sq * sq)
+end
+
+test:is(tonumber(sq), math.fmod(math.pow(42, 8), math.pow(2, 32)))
+
+test:check()
