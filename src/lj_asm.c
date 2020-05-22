71ec8eb232d4dfa8df2cbbae65b799b2ce493979
Mike Pall <mike>
2020-05-22 04:53:35 +0200

    Cleanup math function compilation and fix inconsistencies.
    (cherry picked from commit 5655be4546d9177890c69f0d0accac4773ff0887)
    
    This patch changes 'math_unary', `math_htrig` and `math_atrig`
    to `math_call` for math functions compilation. Now all of
    math functions in IRs are called with `CALLN` instead of
    `FPMATH` (`ATAN2` for `math.atan2`).
    
    The `ATAN2` instruction itself is removed, as well as the fold
    optimization for it.
    
    Also, this patch adds new fold optimizations for `CALLN`.
    
    Maxim Kokryashkin:
    * added the description for the problem
    
    Part of tarantool/tarantool#7230
    
    Reviewed-by: Sergey Kaplun <skaplun@tarantool.org>
    Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

--- src/lj_asm.c
+++ src/lj_asm.c
@@ -1660,7 +1660,6 @@ static void asm_ir(ASMState *as, IRIns *ir)
   case IR_DIV: asm_div(as, ir); break;
   case IR_POW: asm_pow(as, ir); break;
   case IR_ABS: asm_abs(as, ir); break;
-  case IR_ATAN2: asm_atan2(as, ir); break;
   case IR_LDEXP: asm_ldexp(as, ir); break;
   case IR_FPMATH: asm_fpmath(as, ir); break;
   case IR_TOBIT: asm_tobit(as, ir); break;
@@ -2150,11 +2149,6 @@ static void asm_setup_regsp(ASMState *as)
 	as->modset = RSET_SCRATCH;
       break;
 #if !LJ_SOFTFP
-    case IR_ATAN2:
-#if LJ_TARGET_X86
-      if (as->evenspill < 4)  /* Leave room to call atan2(). */
-	as->evenspill = 4;
-#endif
 #if !LJ_TARGET_X86ORX64
     case IR_LDEXP:
 #endif
