b6d285214db9892aaef838950d200f395c93fe2d
Mike Pall <mike>
2021-03-23 00:22:34 +0100

    Cleanup and enable external unwinding for more platforms.
    (cherry picked from commit e131936133c58de4426c595db2341caf5a1665b5)
    
    This commit enables external unwinding on all platforms, that
    create unwind tables by default. Corresponding check is added
    to the build routine.
    
    Also, GC64 mode is now enabled on MacOS by default, as
    non-GC64 mode is forbidden.
    
    Maxim Kokryashkin:
    * added the description for the patch, adjusted CMake machinery and CI
    
    Needed for tarantool/tarantool#6096
    Part of tarantool/tarantool#7230
    
    Reviewed-by: Sergey Kaplun <skaplun@tarantool.org>
    Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

--- cmake/SetTargetFlags.cmake
+++ cmake/SetTargetFlags.cmake
@@ -15,6 +15,24 @@ endif()
 LuaJITTestArch(TESTARCH "${TARGET_C_FLAGS}")
 LuaJITArch(LUAJIT_ARCH "${TESTARCH}")
 
+string(FIND ${TARGET_C_FLAGS} "LJ_NO_UNWIND 1" UNWIND_POS)
+if(UNWIND_POS EQUAL -1)
+  # Find out whether the target toolchain always generates unwind
+  # tables.
+  execute_process(
+    COMMAND bash -c "exec 2>/dev/null; echo 'extern void b(void);int a(void){b();return 0;}' | ${CMAKE_C_COMPILER} -c -x c - -o tmpunwind.o && grep -qa -e eh_frame -e __unwind_info tmpunwind.o && echo E; rm -f tmpunwind.o"
+    WORKING_DIRECTORY ${LUAJIT_SOURCE_DIR}
+    OUTPUT_VARIABLE TESTUNWIND
+    RESULT_VARIABLE TESTUNWIND_RC
+  )
+  if(TESTUNWIND_RC EQUAL 0)
+    string(FIND "${TESTUNWIND}" "E" UNW_TEST_POS)
+    if(NOT UNW_TEST_POS EQUAL -1)
+      AppendFlags(TARGET_C_FLAGS -DLUAJIT_UNWIND_EXTERNAL)
+    endif()
+  endif()
+endif()
+
 # Target-specific compiler options.
 #
 # x86/x64 only: For GCC 4.2 or higher and if you don't intend to
@@ -25,12 +43,6 @@ if(LUAJIT_ARCH STREQUAL "x86")
 endif()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  if(LUAJIT_ARCH STREQUAL "x64")
-    # XXX: Set -pagezero_size to hint <mmap> when allocating 32
-    # bit memory on OSX/x64, otherwise the lower 4GB are blocked.
-    AppendFlags(TARGET_BIN_FLAGS -pagezero_size 10000 -image_base 100000000)
-    AppendFlags(TARGET_SHARED_FLAGS -image_base 7fff04c4a000)
-  endif()
   AppendFlags(TARGET_SHARED_FLAGS -single_module -undefined dynamic_lookup)
 else() # Linux and FreeBSD.
   AppendFlags(TARGET_BIN_FLAGS -Wl,-E)
