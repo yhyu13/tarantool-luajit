f42a5d1762d19ac2335fbb2a97e29d3d577e453f
Mike Pall <mike>
2021-07-22 16:21:45 +0200

    Fix io.close() error message.
    Reported by François Perrad.
    
    (cherry picked from commit d87da3d5fed7f046c426d6a747bb84c2880f4e8d)
    
    When an `io.close()` is called without arguments on an already closed
    default output, the returned error message is `LJ_ERR_IOSTDCL`
    ("standard file is closed") instead of `LJ_ERR_IOCLFL` ("attempt to use
    a closed file"). It is never a "real" standard file, because trying to
    close a standard throws an error ("cannot close standard file"). Also,
    this is inconsistent with PUC Lua.
    
    This patch adds the corresponding check and a code branch for this
    corner case.
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#7230
    
    Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
    Reviewed-by: Maxim Kokryashkin <m.kokryashkin@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

--- test/tarantool-tests/lj-735-io-close-on-closed-file.test.lua
+++ test/tarantool-tests/lj-735-io-close-on-closed-file.test.lua
@@ -1,7 +1,7 @@
 local tap = require('tap')
 
 local test = tap.test('lj-735-io-close-on-closed-file')
-test:plan(1)
+test:plan(2)
 
 local TEST_FILE = 'lj-735-io-close-on-closed-file.tmp'
 
@@ -12,11 +12,12 @@ io.output(TEST_FILE)
 local status, err = io.close()
 assert(status, err)
 
-status = pcall(io.close)
+status, err = pcall(io.close)
 
 io.output(oldstdout)
 
 test:ok(not status, 'close already closed file')
+test:ok(err:match('attempt to use a closed file'), 'correct error message')
 
 assert(os.remove(TEST_FILE))
 
