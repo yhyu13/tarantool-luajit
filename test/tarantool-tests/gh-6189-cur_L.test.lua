7570ff61138fb50a17ed78ee28dde0acbbf6131e
Sergey Kaplun <skaplun@tarantool.org>
2021-08-15 15:47:13 +0300

    arm64: fix cur_L restoration on error throw
    This change is the follow-up of commits
    ed412cd9f55fe87fd32a69c86e1732690fc5c1b0 ('Update cur_L on exceptional
    path') and 97699d9ee2467389b6aea21a098e38aff3469b5f ('Fix cur_L tracking
    on exceptional path').
    
    When an error is thrown on the coroutine that is not the one being
    currently executed, `cur_L` is not set up. Hence, when the running trace
    exits at assertion guard right after the error is caught, Lua state is
    restored from the incorrect `cur_L`. As a result the resulting stack is
    inconsistent and the crash occurs.
    
    Aforementioned patches fix the behaviour only for x86/x64 architectures.
    This patch updates the `cur_L` for arm64 architecture too.
    
    Nevertheless, throwing an error at non-currently executed coroutine is a
    violation of Lua/C API. So, in the nearest possible future this patch
    should be replaced with the corresponding assert in `lj_err_throw()`.
    
    Resolves tarantool/tarantool#6189
    Relates to tarantool/tarantool#6323
    Follows up tarantool/tarantool#1516
    
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    Reviewed-by: Kirill Yukhin <kyukhin@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

---
+++ test/tarantool-tests/gh-6189-cur_L.test.lua
@@ -0,0 +1,28 @@
+local libcur_L = require('libcur_L')
+local tap = require('tap')
+
+local test = tap.test('gh-6189-cur_L')
+test:plan(1)
+
+local function cbool(cond)
+  if cond then
+    return 1
+  else
+    return 0
+  end
+end
+
+-- Compile function to trace with snapshot.
+jit.opt.start('hotloop=1')
+-- First call makes `cbool()` hot enough to be recorded next time.
+cbool(true)
+-- Second call records `cbool()` body (i.e. `if` branch). This is
+-- a root trace for `cbool()`.
+cbool(true)
+
+assert(pcall(libcur_L.error_from_other_thread) == false, "return from error")
+-- Call with restoration from a snapshot with wrong cur_L.
+cbool(false)
+
+test:ok(true)
+os.exit(test:check() and 0 or 1)
