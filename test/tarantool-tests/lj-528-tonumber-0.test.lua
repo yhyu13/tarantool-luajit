6e8373f7cabd4895c10d3f3f90c8a3c60634f5d7
Mike Pall <mike>
2022-01-13 15:38:56 +0100

    Fix tonumber("-0") in dual-number mode.
    Reported by Sergey Kaplun.
    
    (cherry picked from commit 103c29e634d822e0affd7d3ae16a7d8a80c038d3)
    
    For DUALNUM build `tonumber("-0x0")` or `tonumber("-0")` returns 0
    instead -0 due to the STRSCAN_OPT_TOINT option of `lj_strscan_scan()`
    and cast the value to integer with losing information about its sign.
    
    This patch adds special checks to string parsing routines to handle
    these corner cases.
    
    Sergey Kaplun:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#6548
    
    Reviewed-by: Igor Munkin <imun@tarantool.org>
    Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

--- test/tarantool-tests/lj-528-tonumber-0.test.lua
+++ test/tarantool-tests/lj-528-tonumber-0.test.lua
@@ -1,15 +1,9 @@
 local tap = require('tap')
 
--- Test disabled for DUALNUM mode default for some arches.
--- See also https://github.com/LuaJIT/LuaJIT/pull/787.
-require('utils').skipcond(
-  jit.arch ~= 'x86' and jit.arch ~= 'x64',
-  jit.arch..' in DUALNUM mode is clumsy for now'
-)
-
 -- Test file to demonstrate LuaJIT `tonumber('-0')` incorrect
 -- behaviour.
--- See also https://github.com/LuaJIT/LuaJIT/issues/528.
+-- See also https://github.com/LuaJIT/LuaJIT/issues/528,
+-- https://github.com/LuaJIT/LuaJIT/pull/787.
 local test = tap.test('lj-528-tonumber-0')
 test:plan(1)
 
