LuaJIT/LuaJIT

Fix bytecode dump unpatching.
Reported by Christopher Oliver.

src/lj_bcwrite.c
@@ -258,10 +258,7 @@ static void bcwrite_bytecode(BCWriteCtx *ctx, GCproto *pt)
 	p[LJ_ENDIAN_SELECT(0, 3)] = (uint8_t)(op-BC_IFORL+BC_FORL);
       } else if (op == BC_JFORL || op == BC_JITERL || op == BC_JLOOP) {
 	BCReg rd = p[LJ_ENDIAN_SELECT(2, 1)] + (p[LJ_ENDIAN_SELECT(3, 0)] << 8);
-	BCIns ins = traceref(J, rd)->startins;
-	p[LJ_ENDIAN_SELECT(0, 3)] = (uint8_t)(op-BC_JFORL+BC_FORL);
-	p[LJ_ENDIAN_SELECT(2, 1)] = bc_c(ins);
-	p[LJ_ENDIAN_SELECT(3, 0)] = bc_b(ins);
+	memcpy(p, &traceref(J, rd)->startins, 4);
       }
     }
   }

yhyu13/tarantool-luajit

(cherry picked from commit 20ac817a747cf8cab044ae81b09c08d23e34342b)
RET bytecodes are patched to JLOOP bytecode in a function with
up-recursion. During dump those bytecodes should be unpatched to the
original one. It is done by restoring the opcode by subtraction the diff
between JLOOP and ILOOP bytecodes. The restore was done by the erroneous
opcode subtraction, that led to a LOOP bytecode in place of the RET one.
This patch fixes the bytecode unpatching via copy of the original
instruction, that was patched.
Sergey Kaplun:
* added the description and the test for the problem
Part of tarantool/tarantool#6548
Reviewed-by: Sergey Ostanevich <sergos@tarantool.org>
Reviewed-by: Igor Munkin <imun@tarantool.org>
Signed-off-by: Igor Munkin <imun@tarantool.org>

src/lj_bcwrite.c
@@ -219,10 +219,7 @@ static char *bcwrite_bytecode(BCWriteCtx *ctx, char *p, GCproto *pt)
 	q[LJ_ENDIAN_SELECT(0, 3)] = (uint8_t)(op-BC_IFORL+BC_FORL);
       } else if (op == BC_JFORL || op == BC_JITERL || op == BC_JLOOP) {
 	BCReg rd = q[LJ_ENDIAN_SELECT(2, 1)] + (q[LJ_ENDIAN_SELECT(3, 0)] << 8);
-	BCIns ins = traceref(J, rd)->startins;
-	q[LJ_ENDIAN_SELECT(0, 3)] = (uint8_t)(op-BC_JFORL+BC_FORL);
-	q[LJ_ENDIAN_SELECT(2, 1)] = bc_c(ins);
-	q[LJ_ENDIAN_SELECT(3, 0)] = bc_b(ins);
+	memcpy(q, &traceref(J, rd)->startins, 4);
       }
     }
   }