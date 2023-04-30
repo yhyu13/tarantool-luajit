4dec2cca11a2a6c0006f15be14c8807e631b5eea
Mike Pall <mike>
2023-04-30 13:38:29 +0200

    Fix last commit.
    Reported by PluMGMK.
    
    (cherry-picked from commit 224129a8e64bfa219d35cd03055bf03952f167f6)
    
    The `_VMEVENTS` table, where the error handler for GC finalizers
    is set, was not cleared from the stack after the initialization.
    This commit adds stack cleanup.
    
    Maxim Kokryashkin:
    * added the description and the test for the problem
    
    Part of tarantool/tarantool#9145
    
    Reviewed-by: Sergey Kaplun <skaplun@tarantool.org>
    Reviewed-by: Sergey Bronnikov <sergeyb@tarantool.org>
    Signed-off-by: Igor Munkin <imun@tarantool.org>

--- src/lib_aux.c
+++ src/lib_aux.c
@@ -352,6 +352,7 @@ LUALIB_API lua_State *luaL_newstate(void)
     lua_pushcfunction(L, error_finalizer);
     lua_rawseti(L, -2, VMEVENT_HASH(LJ_VMEVENT_ERRFIN));
     G(L)->vmevmask = VMEVENT_MASK(LJ_VMEVENT_ERRFIN);
+    L->top--;
 #endif
   }
   return L;
@@ -378,6 +379,7 @@ LUALIB_API lua_State *luaL_newstate(void)
     lua_pushcfunction(L, error_finalizer);
     lua_rawseti(L, -2, VMEVENT_HASH(LJ_VMEVENT_ERRFIN));
     G(L)->vmevmask = VMEVENT_MASK(LJ_VMEVENT_ERRFIN);
+    L->top--;
 #endif
   }
   return L;
